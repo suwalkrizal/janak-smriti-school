c78f097c54be5b752dd142ddc425684aed3e822f
krijal <krijalsuwal67@gmail.com>
2025-04-24 22:24:21 +0545

    creating serializers for api

--- project/settings.py
+++ project/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'school',
+    'rest_framework',
+    'django_filters',
 ]
 
 MIDDLEWARE = [
---
+++ school/seializers.py
@@ -0,0 +1,60 @@
+from rest_framework import serializers
+from .models import *
+
+
+class BannerSerializer(serializers.ModelSerializer):
+    image = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Banner
+        fields = '__all__'
+    
+    def get_image(self, obj):
+        request = self.context.get('request')
+        if obj.image:
+            return request.build_absolute_uri(obj.image.url) if request else obj.image.url
+        return None
+
+class AboutUsImageSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = AboutUsImage
+        fields = ['id','image']
+
+class AboutUsSerializer(serializers.ModelSerializer):
+    images = serializers.SerializerMethodField()
+    class Meta:
+        model = AboutUs
+        fields = ['id', 'title', 'content','images']
+        
+    def get_images(self, obj):
+        """Return only the first 10 images."""
+        images = obj.images.all()[:10]  # Limit to 10 images
+        return AboutUsImageSerializer(images, many=True).data
+
+class BlogContentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = BlogContent
+        fields = '__all__'
+
+class BlogPostSerializer(serializers.ModelSerializer):
+    sub_titles = BlogContentSerializer(many=True, source='blogcontent_set')
+
+    class Meta:
+        model = BlogPost
+        fields = '__all__'
+
+
+class ContactUsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ContactUs
+        fields = '__all__'  # Include all fields
+
+class TestimonialSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Testimonial
+        fields = '__all__'
+
+class OfferSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Offer
+        fields = '__all__'
